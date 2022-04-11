c974708c69d809f33d3a55b92ecdbb16ad3a23f5
TheDoomForce <malkite1@auxilioabalto.me>
2022-04-10 20:52:40 -0400

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    char * x;
+
+    x = "Apples";
+
+    printf("Hello, %s\n", x);
+
+    int Cooldate;
+
+    Cooldate = 1995 - 11;
+
+    printf("Do You Like %d?", Cooldate);
+
+    return 0;
+}
